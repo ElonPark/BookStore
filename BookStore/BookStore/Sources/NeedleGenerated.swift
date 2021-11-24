194f23b9f8c86639414fca99cef5d31c2f17f165
Elon Park <dev.sungwoon@gmail.com>
2021-11-24 21:36:34 +0900

    feat: Implementation Needle DI

--- BookStore/BookStore/Sources/NeedleGenerated.swift
+++ BookStore/BookStore/Sources/NeedleGenerated.swift
@@ -1,6 +1,8 @@
 
 
-
+import Foundation
+import NeedleFoundation
+import UIKit
 
 // swiftlint:disable unused_declaration
 private let needleDependenciesHash : String? = nil
@@ -8,8 +10,45 @@ private let needleDependenciesHash : String? = nil
 // MARK: - Registration
 
 public func registerProviderFactories() {
+    __DependencyProviderRegistry.instance.registerDependencyProviderFactory(for: "^->RootComponent") { component in
+        return EmptyDependencyProvider(component: component)
+    }
+    __DependencyProviderRegistry.instance.registerDependencyProviderFactory(for: "^->RootComponent->SearchComponent") { component in
+        return SearchDependencyf947dc409bd44ace18e0Provider(component: component)
+    }
+    __DependencyProviderRegistry.instance.registerDependencyProviderFactory(for: "^->RootComponent->SearchComponent->BookDetailsComponent") { component in
+        return BookDetailsDependency099220a05e4493d9bd9fProvider(component: component)
+    }
     
 }
 
 // MARK: - Providers
 
+private class SearchDependencyf947dc409bd44ace18e0BaseProvider: SearchDependency {
+
+
+    init() {
+
+    }
+}
+/// ^->RootComponent->SearchComponent
+private class SearchDependencyf947dc409bd44ace18e0Provider: SearchDependencyf947dc409bd44ace18e0BaseProvider {
+    init(component: NeedleFoundation.Scope) {
+        super.init()
+    }
+}
+private class BookDetailsDependency099220a05e4493d9bd9fBaseProvider: BookDetailsDependency {
+    var isbn13Validator: ISBN13validating {
+        return searchComponent.isbn13Validator
+    }
+    private let searchComponent: SearchComponent
+    init(searchComponent: SearchComponent) {
+        self.searchComponent = searchComponent
+    }
+}
+/// ^->RootComponent->SearchComponent->BookDetailsComponent
+private class BookDetailsDependency099220a05e4493d9bd9fProvider: BookDetailsDependency099220a05e4493d9bd9fBaseProvider {
+    init(component: NeedleFoundation.Scope) {
+        super.init(searchComponent: component.parent as! SearchComponent)
+    }
+}
--- BookStore/BookStore/Sources/Presentation/Scenes/Deatils/BookDetailsBuilder.swift
+++ BookStore/BookStore/Sources/Presentation/Scenes/Deatils/BookDetailsBuilder.swift
@@ -8,54 +8,63 @@
 import Foundation
 import UIKit
 
-final class BookDetailsComponent {
+import NeedleFoundation
 
-  private let isbn13: String
+protocol BookDetailsDependency: Dependency {
+    var isbn13Validator: ISBN13validating { get }
+}
 
-  fileprivate var initialState: BookDetailsModel.Details.State {
-    return BookDetailsModel.Details.State(isbn13: isbn13)
-  }
+final class BookDetailsComponent: Component<BookDetailsDependency> {
 
-  fileprivate var bookDetailsRepository: BookDetailsRepository {
-    return BookDetailsRepositoryImpl(networkProvider: NetworkProvider())
-  }
+    private let isbn13: String
 
-  fileprivate var isbn13Validator: ISBN13validating {
-    return ISBN13Validator()
-  }
+    fileprivate var initialState: BookDetailsModel.Details.State {
+        return BookDetailsModel.Details.State(isbn13: isbn13)
+    }
 
-  init(isbn13: String) {
-    self.isbn13 = isbn13
-  }
+    fileprivate var bookDetailsRepository: BookDetailsRepository {
+        return BookDetailsRepositoryImpl(networkProvider: NetworkProvider())
+    }
+
+    fileprivate var isbn13Validator: ISBN13validating {
+        return dependency.isbn13Validator
+    }
+
+    init(parent: Scope, withISBN13 isbn13: String) {
+        self.isbn13 = isbn13
+        super.init(parent: parent)
+    }
 }
 
 protocol BookDetailsBuildable {
-  func build(withISBN13 isbn13: String) -> UIViewController
+    func build(withISBN13 isbn13: String) -> UIViewController
 }
 
-final class BookDetailsBuilder: BookDetailsBuildable {
+final class BookDetailsBuilder:
+    ComponentizedBuilder<BookDetailsComponent, UIViewController, String>,
+    BookDetailsBuildable
+{
 
-  deinit {
-    print("deinit: \(type(of: self))")
-  }
+    override func build(with component: BookDetailsComponent) -> UIViewController {
+        let router = BookDetailsRouter()
+        let interactor = BookDetailsInteractor(
+            initialState: component.initialState,
+            bookDetailsRepository: component.bookDetailsRepository,
+            isbn13Validator: component.isbn13Validator
+        )
+        let presenter = BookDetailsPresenter()
+        let viewController = BookDetailsViewController()
 
-  func build(withISBN13 isbn13: String) -> UIViewController {
-    let component = BookDetailsComponent(isbn13: isbn13)
-    let router = BookDetailsRouter()
-    let interactor = BookDetailsInteractor(
-      initialState: component.initialState,
-      bookDetailsRepository: component.bookDetailsRepository,
-      isbn13Validator: component.isbn13Validator
-    )
-    let presenter = BookDetailsPresenter()
-    let viewController = BookDetailsViewController()
+        viewController.interacter = interactor
+        interactor.presenter = presenter
+        interactor.router = router
+        router.viewController = viewController
+        presenter.viewController = viewController
 
-    viewController.interacter = interactor
-    interactor.presenter = presenter
-    interactor.router = router
-    router.viewController = viewController
-    presenter.viewController = viewController
+        return viewController
+    }
 
-    return viewController
-  }
+    func build(withISBN13 isbn13: String) -> UIViewController {
+        build(withDynamicComponentDependency: isbn13)
+    }
 }
